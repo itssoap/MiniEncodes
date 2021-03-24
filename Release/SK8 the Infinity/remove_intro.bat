cd8b2bac09825464b96f061821b963a58a655b17
Soaib Khan <soaibkhan38@gmail.com>
2021-03-24 20:16:43 +0530

    Minor bug

--- Release/SK8 the Infinity/remove_intro.bat
+++ Release/SK8 the Infinity/remove_intro.bat
@@ -2,6 +2,6 @@
 
 for %%i in (*.mkv) do "C:/Program Files/MKVToolNix\mkvmerge.exe" --output "release/%%i" --split timestamps:12s --language 1:jpn --track-name 1:Japanese --default-track 1:yes --language 0:und --default-track 0:yes "%%i" --track-order 0:0,0:1
 
-del "*001.mkv"
+del "release/*001.mkv"
 
 pause
