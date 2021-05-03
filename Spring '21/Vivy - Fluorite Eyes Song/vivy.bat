3d15a9a528b5b4e23c638181333870c59ba241c8
Soap <soaibkhan38@gmail.com>
2021-05-03 17:55:42 +0530

    Funi color range fix

--- Spring '21/Vivy - Fluorite Eyes Song/vivy.bat
+++ Spring '21/Vivy - Fluorite Eyes Song/vivy.bat
@@ -25,13 +25,17 @@ echo %r1%
 echo %r2%
 echo %r3%
 
-vspipe --y4m vivy.vpy --arg key="%~1" - | ffmpeg -hide_banner -v quiet -stats -y -f yuv4mpegpipe -i - -s 854x480 -c:v libx265 -x265-params "limit-sao=1:bframes=8:psy-rd=1.5:psy-rdoq=2:aq-mode=3" -crf 22 -pix_fmt yuv420p10le -preset slow -map 0 -movflags faststart "%r1%1.mkv"
-vspipe --y4m vivy.vpy --arg key="%~1" - | ffmpeg -hide_banner -v quiet -stats -y -f yuv4mpegpipe -i - -s 1280x720 -c:v libx265 -x265-params "limit-sao=1:bframes=8:psy-rd=1.5:psy-rdoq=2:aq-mode=3" -crf 22 -pix_fmt yuv420p10le -preset slow -map 0 -movflags faststart "%r2%1.mkv"
-vspipe --y4m vivy.vpy --arg key="%~1" - | ffmpeg -hide_banner -v quiet -stats -y -f yuv4mpegpipe -i - -s 1920x1080 -c:v libx265 -x265-params "limit-sao=1:bframes=8:psy-rd=1.5:psy-rdoq=2:aq-mode=3" -crf 22 -pix_fmt yuv420p10le -preset slow -map 0 -movflags faststart "%r3%1.mkv"
+echo "Fixing Color range"
 
-ffmpeg -hide_banner -v quiet -stats -y -i "%r1%1.mkv" -i "%~1" -map 1 -map -1:v -map 0 -c:v copy -c:a aac -ac 2 -ab 128k -map_metadata:g -1 -metadata title="[AniDL] Vivy - Fluorite Eye's Song [WEB 480p 10bit][Soap]" -metadata:s:v title="" -metadata:s:a title="Japanese" "1%r1%.mkv"
-ffmpeg -hide_banner -v quiet -stats -y -i "%r2%1.mkv" -i "%~1" -map 1 -map -1:v -map 0 -c:v copy -c:a aac -ac 2 -ab 128k -map_metadata:g -1 -metadata title="[AniDL] Vivy - Fluorite Eye's Song [WEB 720p 10bit][Soap]" -metadata:s:v title="" -metadata:s:a title="Japanese" "1%r2%.mkv"
-ffmpeg -hide_banner -v quiet -stats -y -i "%r3%1.mkv" -i "%~1" -map 1 -map -1:v -map 0 -c:v copy -c:a aac -ac 2 -ab 128k -map_metadata:g -1 -metadata title="[AniDL] Vivy - Fluorite Eye's Song [WEB 1080p 10bit][Soap]" -metadata:s:v title="" -metadata:s:a title="Japanese" "1%r3%.mkv"
+ffmpeg -y -hide_banner -v quiet -stats -i "%~1" -c copy -bsf:v h264_metadata=video_full_range_flag=1 -color_range 0 "a%~1"
+
+vspipe --y4m vivy.vpy --arg key="a%~1" - | ffmpeg -hide_banner -v quiet -stats -y -f yuv4mpegpipe -i - -s 854x480 -c:v libx265 -x265-params "limit-sao=1:bframes=8:psy-rd=1.5:psy-rdoq=2:aq-mode=3:deblock=-2,-2" -crf 22 -pix_fmt yuv420p10le -preset slow -map 0 -movflags faststart "%r1%1.mkv"
+vspipe --y4m vivy.vpy --arg key="a%~1" - | ffmpeg -hide_banner -v quiet -stats -y -f yuv4mpegpipe -i - -s 1280x720 -c:v libx265 -x265-params "limit-sao=1:bframes=8:psy-rd=1.5:psy-rdoq=2:aq-mode=3:deblock=-2,-2" -crf 22 -pix_fmt yuv420p10le -preset slow -map 0 -movflags faststart "%r2%1.mkv"
+vspipe --y4m vivy.vpy --arg key="a%~1" - | ffmpeg -hide_banner -v quiet -stats -y -f yuv4mpegpipe -i - -s 1920x1080 -c:v libx265 -x265-params "limit-sao=1:bframes=8:psy-rd=1.5:psy-rdoq=2:aq-mode=3:deblock=-2,-2" -crf 22 -pix_fmt yuv420p10le -preset slow -map 0 -movflags faststart "%r3%1.mkv"
+
+ffmpeg -hide_banner -v quiet -stats -y -i "%r1%1.mkv" -i "a%~1" -map 1 -map -1:v -map 0 -c:v copy -c:a aac -ac 2 -ab 128k -map_metadata:g -1 -metadata title="[AniDL] Vivy - Fluorite Eye's Song [WEB 480p 10bit][Soap]" -metadata:s:v title="" -metadata:s:a title="Japanese" "1%r1%.mkv"
+ffmpeg -hide_banner -v quiet -stats -y -i "%r2%1.mkv" -i "a%~1" -map 1 -map -1:v -map 0 -c:v copy -c:a aac -ac 2 -ab 128k -map_metadata:g -1 -metadata title="[AniDL] Vivy - Fluorite Eye's Song [WEB 720p 10bit][Soap]" -metadata:s:v title="" -metadata:s:a title="Japanese" "1%r2%.mkv"
+ffmpeg -hide_banner -v quiet -stats -y -i "%r3%1.mkv" -i "a%~1" -map 1 -map -1:v -map 0 -c:v copy -c:a aac -ac 2 -ab 128k -map_metadata:g -1 -metadata title="[AniDL] Vivy - Fluorite Eye's Song [WEB 1080p 10bit][Soap]" -metadata:s:v title="" -metadata:s:a title="Japanese" "1%r3%.mkv"
 
 "C:/Program Files/MKVToolNix\mkvmerge.exe" --output "%r1%.mkv" --language 1:jpn --track-name 1:Japanese --default-track 1:yes --sub-charset 2:UTF-8 --language 2:eng --track-name ^"2:English subs^" --default-track 2:yes --language 0:und --default-track 0:yes ^"1%r1%.mkv^" --track-order 0:0,0:1,0:2
 "C:/Program Files/MKVToolNix\mkvmerge.exe" --output "%r2%.mkv" --language 1:jpn --track-name 1:Japanese --default-track 1:yes --sub-charset 2:UTF-8 --language 2:eng --track-name ^"2:English subs^" --default-track 2:yes --language 0:und --default-track 0:yes ^"1%r2%.mkv^" --track-order 0:0,0:1,0:2
